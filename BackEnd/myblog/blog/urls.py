2c0d14738dcb3ec5e985358f8ad3593a5d7d5bce
Jungsik <sk7556@naver.com>
2023-11-27 00:31:10 +0900

    study file

--- BackEnd/myblog/blog/urls.py
+++
@@ -1,8 +0,0 @@
-# blog/urls.py
-from django.urls import path
-from .views import PostListCreateView, PostDetailView
-
-urlpatterns = [
-    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-]
