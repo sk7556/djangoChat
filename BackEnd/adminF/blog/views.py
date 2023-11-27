7cbeb96b4eeeeeb6a0b5a21201caacbab795b01a
Jungsik <sk7556@naver.com>
2023-11-27 22:30:34 +0900

    commit

---
+++ BackEnd/adminF/blog/views.py
@@ -0,0 +1,43 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/변경.html'
+
+class PostDetail(DetailView):
+    model = Post
+
+class PostCreate(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('postlist')
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('postlist')
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('postlist') # 삭제되고 처리가 다 완료되지 않았을 수 있으니 지연 필요한 reverse_lazy를 사용함.
+
+class PostTest(CreateView):
+    model = Post
+    
+    def get(self, request):
+        return HttpResponse('PostTest get')
+    
+    def post(self, request):
+        return HttpResponse('PostTest post')
+    
+
+postlist = PostList.as_view() # as_view는 진입 메소드
+postdetail = PostDetail.as_view()
+write = PostCreate.as_view()
+edit = PostUpdate.as_view()
+delete = PostDelete.as_view()
+test = PostTest.as_view()
