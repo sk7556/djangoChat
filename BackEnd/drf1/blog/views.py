6a5b38e3812e3b551b636c82c88ca092c8b73664
Jungsik <sk7556@naver.com>
2023-11-30 00:00:36 +0900

    for github frontpage

--- BackEnd/drf1/blog/views.py
+++ BackEnd/drf1/blog/views.py
@@ -1,35 +1,20 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+# rest_framework 추가 후 추가된 코드
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 
-# def postlist(request):
-#     posts = Post.objects.all()
-#     return render(request, 'blog/postlist.html', {'posts':posts})
 
-
-# def postlist(request):
-#     posts = [
-#         {'title':'1', 'content':'111'},
-#         {'title':'2', 'content':'222'},
-#         {'title':'3', 'content':'333'},
-#     ]
-#     return render(request, 'blog/postlist.html', {'posts':posts})
-
-# def postlist(request):
-#     posts = [
-#         {'title':'1', 'content':'111'},
-#         {'title':'2', 'content':'222'},
-#         {'title':'3', 'content':'333'},
-#     ]
-#     return JsonResponse(posts, safe=False) # dictionary이외를 받을 경우, safe=False로 설정
-
-# 호기심에 의한 테스트
-# def postlist(request):
-#     posts = list(Post.objects.all()) # post는 serializable하지 않습니다.
-#     return JsonResponse(posts, safe=False) # dictionary이외를 받을 경우, safe=False로 설정
-
+# FBV 사용하는 방식
+@api_view(['GET']) # ['GET', 'POST']하면 둘 다 처리 가능
 def postlist(request):
-    posts = []
-    for i in Post.objects.all():
-        posts.append({'title':i.title, 'content':i.content})
-    return JsonResponse(posts, safe=False) # dictionary이외를 받을 경우, safe=False로 설정
+    posts = [
+        {'title':'1', 'content':'111'},
+        {'title':'2', 'content':'222'},
+        {'title':'3', 'content':'333'},
+    ]
+    serializer = posts # 직렬화 하는 단계
+    return Response(serializer) # Response로 반환 되었을 때 데이터를 읽을 수도 있고, POST를 보낼 수도 있습니다.
