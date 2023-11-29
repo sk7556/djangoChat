e1a05750ac1cda50b83ffa20a2e4bd6bae93f10c
Jungsik <sk7556@naver.com>
2023-11-30 00:00:49 +0900

    for FrontEnd

--- BackEnd/drf1/blog/views.py
+++ BackEnd/drf1/blog/views.py
@@ -6,15 +6,18 @@ from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from .serializers import PostSerializer
 
 
-# FBV 사용하는 방식
-@api_view(['GET']) # ['GET', 'POST']하면 둘 다 처리 가능
+@api_view(['GET', 'POST'])
 def postlist(request):
-    posts = [
-        {'title':'1', 'content':'111'},
-        {'title':'2', 'content':'222'},
-        {'title':'3', 'content':'333'},
-    ]
-    serializer = posts # 직렬화 하는 단계
-    return Response(serializer) # Response로 반환 되었을 때 데이터를 읽을 수도 있고, POST를 보낼 수도 있습니다.
+    if request.method == 'GET':
+        postlist = Post.objects.all()
+        serializer = PostSerializer(postlist, many=True) # 다수의 Queryset을 넘길 때는 many=True
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
